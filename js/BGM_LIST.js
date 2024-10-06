b22a8eb09db3034bffa3c35719fe8f66f178f900
CNZW <2429671588@qq.com>
2024-10-06 22:40:49 +0800

    Add files via upload

--- js/BGM_LIST.js
+++ js/BGM_LIST.js
@@ -8,8 +8,13 @@ var BGM_list = {
     7: "登录的小曲.ogg",
     8: "希望の朝へ.m4a",
     9: "unwelcome-school.ogg",
-    10: "優しさの記憶.m4a",
-    11: "澄んだ青空、萌ゆる心.m4a",
-    12: "青色コントラスト.m4a",
-    13: "青空Magic_Day-中.m4a"
+    10: "Connected_Sky.m4a",
+    11: "優しさの記憶.m4a",
+    12: "澄んだ青空、萌ゆる心.m4a",
+    13: "青色コントラスト.m4a",
+    14: "青空Magic_Day-中.m4a",
+    15: "After_the_Beep.m4a",
+    16: "Shooting_Stars.m4a",
+    17: "Future_Bossa.m4a",
+    18: "Midsummer_cat.m4a"
 };
