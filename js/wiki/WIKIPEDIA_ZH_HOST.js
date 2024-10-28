c14abcb466dfc65b0b7fc9d06b8917e3076280ef
CNZW <2429671588@qq.com>
2024-10-28 21:55:14 +0800

    Add files via upload

--- js/wiki/WIKIPEDIA_ZH_HOST.js
+++ js/wiki/WIKIPEDIA_ZH_HOST.js
@@ -1,3 +1,7 @@
 var wikipedia_zh_mirror_host = {
-    1: 'zh.wikipedia.cdn.cnzw.us.kg'
+    1: 'zh.wikipedia.cdn.cnzw.us.kg',
+    2: 'zh-IIIIlllllllIIIlIlIIlIIIllIl.wikipedia.cdn.cnzw.us.kg',
+    3: 'zh-IIIIllllIIIlllllIIIllllIlII.wikipedia.cdn.cnzw.us.kg',
+    4: 'zh-IIIllIllllIIIlllllIIIIllllI.wikipedia.cdn.cnzw.us.kg',
+    5: 'zh-IlIlllIlIIllIIIIIllIlIIlllI.wikipedia.cdn.cnzw.us.kg'
 }
