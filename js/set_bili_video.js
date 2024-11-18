71462dae17a1dfd096300fb322f79cbf0fd38a10
CNZW <2429671588@qq.com>
2024-11-18 23:43:53 +0800

    Add files via upload

---
+++ js/set_bili_video.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+    var bili_id_list = document.querySelectorAll("video[data-bili-id]"),
+        bili_id_list_m = 0;
+    setbilivideourl();
+    function setbilivideourl() {
+        if (bili_id_list_m < bili_id_list.length) {
+            if (bili_id_list[bili_id_list_m].getAttribute('data-bili-p') == null) {
+                bili_id_list[bili_id_list_m].setAttribute('data-bili-p', 1);
+            }
+            fetch(`https://api.dyqvideo.com/download/url/get`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "url": `https://www.bilibili.com/video/${bili_id_list[bili_id_list_m].getAttribute('data-bili-id')}`,
+                    "videoChannel": "BILI_BILI"
+                })
+            })
+                .then(response => response.json())
+                .then(CNZW => {
+                    bili_id_url = CNZW['data']['downloadUrl'];
+                    bili_id_list[bili_id_list_m].innerHTML = `<source src="${CNZW['data']['downloadUrl']}">`;
+                    bili_id_list[bili_id_list_m].setAttribute('src', CNZW['data']['downloadUrl']);
+                    bili_id_list_m += 1;
+                    setbilivideourl();
+                })
+        }
+    }
+}
