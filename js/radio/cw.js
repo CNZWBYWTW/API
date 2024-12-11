19c7a6f982b2ddcd7642e51f3dc0914613698b5c
CNZW <2429671588@qq.com>
2024-12-11 21:24:38 +0800

    Update cw.js

--- js/radio/cw.js
+++ js/radio/cw.js
@@ -1862,6 +1862,5 @@ function cw(code) {
     m.setText(code);
     m.renderPlayer('cw_player', m);
     document.querySelector("#cw_player").style = "display: none;";
-    document.querySelector("img[title='Stop']").click();
     document.querySelector("button[title='Play / Pause']").click();
 }
